1f2b07f7633e705fbe1aa39dbc019acfde0fe42f
satyajit090 <beherasatyajit090@gmail.com>
2021-09-30 11:26:41 +0530

    first comment

---
+++ src/test/java/com/example/chatappsatya/ChatAppSatyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatappsatya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppSatyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
